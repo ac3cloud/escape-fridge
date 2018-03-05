71346e11a0674542ec2ee935c67e963b737447bb
John Ferlito <johnf@inodes.org>
2018-03-05 19:07:04 +1100

    Add some layout

--- pi/routes/index.js
+++ pi/routes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', (req, res /* , next */) => {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
