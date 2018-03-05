d6ff6bbad4f8714a8e9ed638051b55890d9c468e
John Ferlito <johnf@inodes.org>
2018-03-05 17:57:24 +1100

    Add eslint

---
+++ pi/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-console': ['error', { allow: ['error', 'warn', 'dir'] }],
+    'max-len': ['error', 180]
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
