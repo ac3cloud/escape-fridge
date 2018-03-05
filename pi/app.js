912655cf64aa33d98e4f1b61b90e78c292c178d2
John Ferlito <johnf@inodes.org>
2018-03-05 19:34:41 +1100

    reload app when code changes

--- pi/app.js
+++ pi/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const reload = require('reload');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+reload(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
