87f34d49295395e0c9d5381f6dddda0c3557981d
John Ferlito <johnf@inodes.org>
2018-03-30 14:19:10 +1100

    Add leaderboard

--- pi/routes/leaderboard.js
+++ pi/routes/leaderboard.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-param-reassign */
+
 const express = require('express');
+const moment = require('moment');
 
 const api = require('../api/leaderboard');
 
@@ -7,7 +10,35 @@ const router = express.Router();
 router.get('/', (req, res /* , next */) => {
   api.getLeaderboard()
     .then((leaderboard) => {
-      console.error(leaderboard);
+      let counter = 1;
+
+
+      leaderboard.forEach((entry) => {
+        entry.diff = (entry.endTime ? entry.endTime : Date.now()) - entry.startTime;
+        entry.time = moment.utc(entry.diff).format('mm:ss');
+      });
+
+      leaderboard.sort((a, b) => a.diff - b.diff);
+
+      leaderboard.forEach((entry) => {
+        entry.rank = counter;
+
+        if (entry.diff > 10 * 60 * 1000) {
+          entry.class = 'table-danger';
+        }
+
+        if (entry.rank === 1) {
+          entry.class = 'table-success';
+        }
+
+        if (!entry.endTime && entry.diff < 10 * 60 * 1000) {
+          entry.time += ' (Now Playing)';
+          entry.class = 'table-primary';
+        }
+
+        counter += 1;
+      });
+
       res.render('leaderboard', { leaderboard });
     });
 });
