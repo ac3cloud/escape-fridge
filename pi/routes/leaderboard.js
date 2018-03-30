5795a53c9ed9acbf5c856c480b57d94ffef6f544
John Ferlito <johnf@inodes.org>
2018-03-30 13:31:19 +1100

    fix leaderboard stop

--- pi/routes/leaderboard.js
+++ pi/routes/leaderboard.js
@@ -5,19 +5,10 @@ const api = require('../api/leaderboard');
 const router = express.Router();
 
 router.get('/', (req, res /* , next */) => {
-  const { email } = req.cookies;
-
-  if (!email) {
-    res.render('error', { message: 'User not logged in', error: {} });
-    return;
-  }
-
-  req.app.locals.email = email;
-
-  api.getLeaderboardByEmail(email)
+  api.getLeaderboard()
     .then((leaderboard) => {
       console.error(leaderboard);
-      res.render('challenge', { leaderboard });
+      res.render('leaderboard', { leaderboard });
     });
 });
 
