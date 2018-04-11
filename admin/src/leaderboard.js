d28e39288ad456ef87a25c8ad456f77bdc3308a2
Greg Cockburn <gergnz@gmail.com>
2018-04-12 06:29:05 +1000

    Revert "remove commit"
    This reverts commit 9d64212e8d2f24c0b844be8f0eeaac0b5d9725ba.

--- admin/src/leaderboard.js
+++ admin/src/leaderboard.js
@@ -117,9 +117,11 @@ module.exports.stop = (event, context, callback) => {
 
 module.exports.get = (event, context, callback) => {
   let email;
+  let startat;
 
   if (event.queryStringParameters) {
     ({ email } = event.queryStringParameters);
+    ({ startat } = event.queryStringParameters);
   }
 
   const params = {
@@ -129,6 +131,11 @@ module.exports.get = (event, context, callback) => {
   if (email) {
     params.FilterExpression = 'email = :email';
     params.ExpressionAttributeValues = { ':email': email };
+  }
+  // Dirty hack, if you supply both this will be a big pile of #WTF?
+  if (startat) {
+    params.FilterExpression = 'startTime > :startTime';
+    params.ExpressionAttributeValues = { ':startTime': parseInt(startat, 10) };
   }
 
   docClient.scan(params).promise()
